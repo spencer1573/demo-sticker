7b5c0aa700e069443b714a1fbf4cd38c93d05646
spencer1573 <spencer1573@users.noreply.github.com>
2022-11-20 16:04:59 -0700

    initial commit

---
+++ sticker/version.py
@@ -0,0 +1,6 @@
+# Generated in setup.py
+
+git_tag = None
+git_revision = 'f59406a4'
+version = '0.1.0+dev.f59406a4'
+linkified_version = '0.1.0+dev.[f59406a4](https://github.com/maunium/stickerpicker/commit/f59406a47a6778cd402e656ffb64f667335f665a)'
